c6ee1d025977b9aba2230b9a762935f9286c149a
LozyOne <bombonek2@mail.ru>
2026-08-05 22:38:50 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -4,10 +4,21 @@ let localStream = null;
 let screenStream = null;
 let peerConnections = {};
 
+// Используем публичные STUN и бесплатный TURN сервер для обхода NAT и мобильных сетей
 const rtcConfig = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' },
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
     ]
 };
 
