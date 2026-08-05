ba97310d7c0b686c6e9b127c27445af0524f4260
LozyOne <bombonek2@mail.ru>
2026-08-05 21:47:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// Раздаем статику из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
 const DATA_FILE = path.join(__dirname, 'data.json');
